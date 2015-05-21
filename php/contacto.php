af6026f4bff430e5e5b06792e8798c16f0d72336
Janai <janaigus@gmail.com>
2015-05-21 19:01:54 +0200

    Añadidas pruebas facebook, reparado errores y cambio de dominio

--- php/contacto.php
+++ php/contacto.php
@@ -4,12 +4,12 @@ $email = $_POST['emailContacto'];
 $asunto = $_POST['asuntoContacto'];
 $texto = $_POST["mensajeContacto"];
 
-$para      = 'janaigus@gmail.com, info@helptoknow.esy.es,'.$email;
+$para      = 'janaigus@gmail.com, info@helptoknow.hol.es,'.$email;
 $titulo    = 'Solicitud de contacto Help To Know "'.$asunto.'"';
 $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
 $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $cabeceras .= 'From: '.$email.'' . "\r\n" .
-    'Reply-To: info@helptoknow.esy.es' . "\r\n" .
+    'Reply-To: info@helptoknow.hol.es' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 $mensaje = '
@@ -18,7 +18,7 @@ $mensaje = '
   <title>Solicitud de información</title>
 </head>
 <body>
-  <h1>Mensaje enviado: <h1>
+  <h1>Mensaje enviado: </h1>
   <p>'.$texto.'</p>
 </body>
 </html>
