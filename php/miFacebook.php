2e046f020b802f433ab3b03b417425ccbcb78330
Janai <janaigus@gmail.com>
2015-05-22 22:16:50 +0200

    Cambios en inicio de sesion con facebok

--- php/miFacebook.php
+++ php/miFacebook.php
@@ -5,6 +5,7 @@ require __DIR__ . './lib/facebook-php-sdk-v4/autoload.php';
 // Make sure to load the Facebook SDK for PHP via composer or manually
 
 use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper
 // add other classes you plan to use, e.g.:
 // use Facebook\FacebookRequest;
 // use Facebook\GraphUser;
@@ -12,5 +13,18 @@ use Facebook\FacebookSession;
 
 FacebookSession::setDefaultApplication('1114829315199633', '15bb057040dc852580e2aa07e1036dce');
 
-
+// Add `use Facebook\FacebookRedirectLoginHelper;` to top of file
+$helper = new FacebookRedirectLoginHelper();
+try {
+    $session = $helper->getSessionFromRedirect();
+} catch(FacebookRequestException $ex) {
+    // When Facebook returns an error
+} catch(\Exception $ex) {
+    // When validation fails or other local issues
+}
+if ($session) {
+    // Logged in
+    // Add `use Facebook\FacebookSession;` to top of file
+    $session = new FacebookSession('access token here');
+}
 ?>
