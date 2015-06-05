166eacb58b6e24380a9d7c412dda065f34a14f98
Janai <janaigus@gmail.com>
2015-06-05 12:52:42 +0200

    Comenzando parte de usuario

--- php/paginas/administrador.php
+++ php/paginas/administrador.php
@@ -68,6 +68,9 @@
             <li>
                 <a href="#actividades" onclick = $("#menu-close").click(); >Actividades</a>
             </li>
+            <li>
+                <a href="#usuarios" onclick = $("#menu-close").click(); >Usuarios</a>
+            </li>
             <hr>
             <?php
             echo'
@@ -367,7 +370,81 @@ echo '
           </div>
         </div>
     </section>
-    
+       
+    <section id="usuarios" class="portfolio">
+        <div class="container">
+          <h1 class="page-header text-center">Usuarios</h1>
+          <div class="row">
+              <div class="col-md-12" style="color:black;">
+                  <div class="thumbnail" style="height: 350px;overflow: auto;" id="grupoVotos">
+                      <div class="row text-center" style="margin: 10px 10px 10px 15px;color:rgb(0,122,135);">
+                            <div class="col-xs-2 col-lg-1"  style="margin: 6px 0px 6px 0px;">
+                                Actividad
+                            </div>
+                            <div class="col-xs-2 col-lg-1" style="margin: 6px 0px 6px 0px;">
+                                Usuario
+                            </div>
+                            <div class="col-xs-2 col-lg-4" style="margin: 6px 0px 6px 0px;">
+                                Votos
+                            </div>
+                            <div class="col-xs-2 col-lg-2" style="margin: 6px 0px 6px 0px;">
+                                Fecha
+                            </div>
+                            <div class="col-xs-2 col-lg-2" style="margin: 6px 0px 6px 0px;">
+                                Editar
+                            </div>
+                            <div class="col-xs-2 col-lg-2" style="margin: 6px 0px 6px 0px;">
+                                Borrar
+                            </div>
+                        </div>
+<?php
+// Crear todas las filas necesarias para los comentarios
+$consulta =  "SELECT vot.id, vot.valoracion, DATE_FORMAT( vot.created,  '%d-%m-%Y a las %k:%i' ) ";
+$consulta .= " AS fecha, vot.idusuario, vot.idactividad, usr.nick AS nick ";
+$consulta .= "FROM votos vot ";
+$consulta .= "INNER JOIN usuarios usr ON vot.idusuario = usr.id ";
+$consulta .= "ORDER BY vot.created DESC ";
+$result = $db->prepare($consulta);
+$result->execute();
+$arrayResult = $result->fetchAll();
+
+for($i = 0;$i < count($arrayResult);$i++){
+echo '
+<div class="row text-center" style="margin: 10px 10px 10px 10px; border: 1px solid #ccc; border-radius: 4px;">
+    <div class="col-xs-12 col-lg-1"  style="margin: 12px 0px 6px 0px;">
+        <a href="./actividad.php?actividad='.$arrayResult[$i]['idactividad'].'">'.$arrayResult[$i]['idactividad'].'</a>
+    </div>
+    <div class="col-xs-12 col-lg-1" style="padding: 12px 0px 6px 0px;">
+        <div>'.$arrayResult[$i]['nick'].'</div>
+    </div>
+    <div class="col-xs-12 col-lg-4" style="padding: 6px 0px 6px 15px;">
+        <select id="selectVoto_'.$arrayResult[$i]['id'].'" class="form-control" disabled="disabled">
+          <option value="1" '.(($arrayResult[$i]['valoracion'] == 1) ? "selected='selected'" : "").'>1</option>
+          <option value="2" '.(($arrayResult[$i]['valoracion'] == 2) ? "selected='selected'" : "").'>2</option>
+          <option value="3" '.(($arrayResult[$i]['valoracion'] == 3) ? "selected='selected'" : "").'>3</option>
+          <option value="4" '.(($arrayResult[$i]['valoracion'] == 4) ? "selected='selected'" : "").'>4</option>
+          <option value="5" '.(($arrayResult[$i]['valoracion'] == 5) ? "selected='selected'" : "").'>5</option>
+        </select>
+    </div>
+    <div class="col-xs-12 col-lg-2" style="padding: 12px 0px 6px 0px;">
+        <div>'.$arrayResult[$i]['fecha'].'</div>
+    </div>
+    <div class="col-xs-12 col-lg-2" style="padding: 8px 0px 6px 0px;">
+        <button id="confirmarEditarVoto_'.$arrayResult[$i]['id'].'" class="btn btn-sm btn-light" style="display: none;"><span class="glyphicon glyphicon-save"></span> Guardar </button>
+        <button id="editarVoto_'.$arrayResult[$i]['id'].'" class="btn btn-sm btn-light"><span class="glyphicon glyphicon-edit"></span> Editar </button>
+    </div>
+    <div class="col-xs-12 col-lg-2" style="padding: 8px 0px 6px 0px;">
+        <button id="borrarVoto_'.$arrayResult[$i]['id'].'" class="btn btn-sm btn-light"><span class="glyphicon glyphicon-trash"></span> Borrar </button>
+    </div>
+</div>
+';
+}
+?>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </section>
     <!-- Modal Información-->
     <div class="modal fade" id="modalWarning" role="dialog">
         <div class="modal-dialog">
