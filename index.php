daec87ee421b60ff4fd714df8413f575108df658
Janai <janaigus@gmail.com>
2015-06-09 20:20:00 +0200

    Arreglado el inicio de sesion con facebook. EL problema era que acada vez que se llamaba al script, la funcion se ejecutaba y producia un inicio de sesion en el instante, ahora solo lo hará al presionar el boton, y registrará en el caso de que sea necesario

--- index.php
+++ index.php
@@ -39,11 +39,10 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <?php
-    if($sesionId == ""){
-        echo '<script type="text/javascript" src="js/social.js"></script>';
-    }
-    ?>
+    
+    <script type="text/javascript" src="js/social.js"></script>
+    
+    
     <!-- Api de google para recaptcha -->
     <script src='https://www.google.com/recaptcha/api.js'></script>
     
