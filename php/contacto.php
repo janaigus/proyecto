26e99dfd4da79d983bb6e32ea12a7ab30f1b6f34
Janai <janaigus@gmail.com>
2015-06-12 19:12:31 +0200

    Se han vuelto a maquetar los correos

--- php/contacto.php
+++ php/contacto.php
@@ -13,15 +13,25 @@ $cabeceras .= 'From: '.$email.'' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 $mensaje = '<!DOCTYPE html><html lang="es">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                </head>
                 <body>
                     <div>
                         <img src="http://helptoknow.esy.es/logo" alt="Logo" width="200" height="110">
                     </div>
+                    <hr/>
                     <div>
                         <h3>Ha enviado una solicitud de contacto</h3>
                     </div>
                     <div>
+                        <hr>
+                        <p>
+                        <h3>Mensaje: </h3>
+                        </p>
                         <p>'.$texto.'</p>
+                        <hr>
                     </div>
                         <h4><strong>Help to know</strong></h4>
                     <p>P. SHERMAN, CALLE WALLABY 42,<br>SYDNEY, AU 90210</p>
--- php/sesion/registrosocial.php
+++ php/sesion/registrosocial.php
@@ -67,56 +67,25 @@
                         <head>
                             <meta charset="utf-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1">
-                            <!-- Bootstrap Core CSS -->
-                            <link href="http://helptoknow.esy.es/bootstrap" rel="stylesheet">
                         </head>
                         <body>
-                              <div class="jumbotron">
-                                    <div class="container">
-                                    <div class="row">
-                                        <div class="col-lg-4 col-lg-offset-4 text-center">
-                                            <img src="http://helptoknow.esy.es/logo" alt="Logo" width="200" height="110">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-4 col-lg-offset-4 text-center">
-                                            <h3>Se ha registrado correctamente</h3>
-                                        </div>
-                                    </div> 
-                                    </div>
-                              </div>
-                              <div class="container">
-                                <div class="row">
-                                    <div class="col-lg-12 text-center">
-                                        <div class="jumbotron" >
-                                            <p>Ha sido registrado satisfactoriamente en Help to Know</p><br/>
-                                  <p>Datos de acceso:</br>Email: '.$_POST['registroEmail'].'</br>Nick dentro de la página: '.$_POST['registroNick'].'
-                                  </p>
-
-                                          </div>
-                                    </div>
-                                </div> 
-                              </div>
-                                <!-- Footer -->
-                            <footer>
-                                <div class="container" >
-                                    <div class="row">
-                                        <div class="col-lg-12 text-center">
-                                            <h4><strong>Help to know</strong>
-                                            </h4>
-                                            <p>P. SHERMAN, CALLE WALLABY 42,<br>SYDNEY, AU 90210</p>
-                                            <ul class="list-unstyled">
-                                                <li><i class="fa fa-phone fa-fw"></i> (123) 456-7890</li>
-                                                <li><i class="fa fa-envelope-o fa-fw"></i><a href="mailto:info@helptoknow.esy.es">info@helptoknow.esy.es</a>
-                                                </li>
-                                            </ul>
-                                            <br>
-                                            <hr class="small">
-                                            <p class="text-muted">Copyright &copy; Help to Know! 2015</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </footer>
+                            <div>
+                                <img src="http://helptoknow.esy.es/logo" alt="Logo" width="200" height="110">
+                            </div>
+                            <div>
+                                <h3>Se ha registrado correctamente</h3>
+                            </div>
+                            <hr>
+                            <div>
+                              <p>Ha sido registrado satisfactoriamente en Help to Know</p>
+                                  <p>Datos de acceso:</p>
+                                  <p>Email: '.$_POST['registroEmail'].'</p>
+                                  <p>Nick dentro de la página: '.$_POST['registroNick'].'</p>
+                            </div>
+                            <hr>
+                                <h4><strong>Help to know</strong></h4>
+                                <p>P. SHERMAN, CALLE WALLABY 42,<br>SYDNEY, AU 90210</p>
+                                <a href="mailto:info@helptoknow.esy.es">info@helptoknow.esy.es</a>
                         </body>
                         </html>';
                         mail($para, $titulo, $mensaje, $cabeceras);
