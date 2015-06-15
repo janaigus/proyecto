1804d8403d42d7e129f52b97a17d39f33d61aee0
janaigus <janaigus@gmail.com>
2015-06-15 20:10:46 +0100

    Cambiado el footer a página individual

--- php/paginas/busqueda.php
+++ php/paginas/busqueda.php
@@ -290,34 +290,7 @@
 
 
     <!-- Footer -->
-    <footer>
-        <div id="contacto" class="container">
-            <div class="row">
-                <div class="col-lg-10 col-lg-offset-1 text-center">
-                    <h4><strong>Help to know</strong>
-                    </h4>
-                    <p>P. SHERMAN, CALLE WALLABY 42,<br>SYDNEY, AU 90210</p>
-                    <ul class="list-unstyled">
-                        <li><i class="fa fa-phone fa-fw"></i> (123) 456-7890</li>
-                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="#modalContacto" id="btnContacto">Contacto</a>
-                        <li><span><a href="">Uso de la web</a></span><span> / <a href="#">Ayuda</a></span></li>
-                        </li>
-                    </ul>
-                    <br>
-                    <ul class="list-inline">
-                        <li>
-                            <a href="https://www.facebook.com/pages/Help-to-know/1615724538676574?ref=hl"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/?lang=es"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
-                        </li>
-                    </ul>
-                    <hr class="small">
-                    <p class="text-muted">Copyright &copy; Help to Know! 2015</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php require('./footer.php'); ?> 
 
     <!-- Incluir los modales necesarios-->
     <?php require('../modales/login.php'); ?>
